ed8f6d2e7f622203b4c64fed8660264263015a56
Mahfuz Bin Minhaz <mahfuz@duck.com>
2022-10-29 21:19:29 +0600

    written successfully

--- pattern.c
+++ pattern.c
@@ -3,14 +3,27 @@ int main(void)
 {
     int n;
     scanf("%d", &n);
+	int count=-1;
     
     int p;
     for(int i=n; i>=1; i--)
     {
-        for(int j=n,p=0; j>=i; j--,p++)
+			count++;
+        for(int j=n; j>=i; j--)
         {
             printf("%d ", j);
-        }
+			if(j==i)
+			{
+				p=2*j-3;
+				for(int k=0;k<p;k++)
+					printf("%d ",j);
+			}
+			if(j==i)
+			{
+				for(int b=n-count;b<=n; b++)
+					if(b!=1)	printf("%d ",b);
+        	}
+		}
         printf("\n");
     }
     for(int i=2; i<=n; i++)
@@ -18,7 +31,18 @@ int main(void)
         for(int j=n; j>=i; j--)
         {
             printf("%d ", j);
+		if(j==i)
+			{
+				p=2*j-3;
+				for(int k=0;k<p;k++)
+					printf("%d ",j);
+			}
+		if(j==i)
+			for(int a=j;a<=n;a++)
+				printf("%d ", a);
+
         }
         printf("\n");
     }
 }
+
